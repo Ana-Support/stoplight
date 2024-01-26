26c39d74114ffe72c701abe2aaf877f4d50fec4b
Ana Hernandez <ana.hernandez@smartbear.com>
2024-01-25 19:01:53 -0600

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMzAxMA/spectral.js?branch_slug=master&token=16c223f3-bdd2-4d77-aeb7-65f5c8fbfa4c";
+export default { extends: ruleset };
